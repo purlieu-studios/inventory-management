da18315b91fbdf9cf4217e785feaef56f4c22038
purlieu-studios <purliestudios.develop@gmail.com>
2025-07-08 19:43:24 -0700

    chore: Get NSwag hooked up

---
+++ src/Blazor/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿namespace InventoryManagement;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger, IHostEnvironment env)
+{
+    private static readonly JsonSerializerOptions _serializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception occurred");
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = "An unexpected error occurred",
+                Status = (int)HttpStatusCode.InternalServerError,
+                Instance = context.Request.Path,
+                Detail = env.IsDevelopment() ? ex.ToString() : null,
+                Type = "https://httpstatuses.com/500"
+            };
+
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode = problemDetails.Status.Value;
+
+            var json = JsonSerializer.Serialize(problemDetails, _serializerOptions);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
--- src/Blazor/Program.cs
+++ src/Blazor/Program.cs
@@ -1,10 +1,30 @@
+using Infrastructure;
+using InventoryManagement;
 using InventoryManagement.Components;
+using Microsoft.EntityFrameworkCore;
+using Seeding;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = "Inventory Management Api";
+    config.Version = "v1";
+    config.Description = "API for Inventory Management";
+    config.DocumentName = "v1";
+});
+builder.Services.AddControllers();
+
+
+builder.Services.AddScoped<ISeedService, SeedService>();
+builder.Services.AddDbContext<MainDbContext>(options =>
+    options.UseInMemoryDatabase("PortfolioDemoDb"));
+
+
 
 var app = builder.Build();
 
@@ -15,14 +35,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+app.UseOpenApi();
+app.UseSwaggerUi();
 app.Run();
